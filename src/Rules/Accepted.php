7d7c5d5a9db139a22b99eba27be8d1d95df50d95
Muhammad Syifa <emsifa@gmail.com>
2016-12-01 22:10:54 +0700

    add rule accepted

---
+++ src/Rules/Accepted.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Rakit\Validation\Rules;
+
+use Rakit\Validation\Rule;
+
+class Accepted extends Rule
+{
+
+    protected $message = "The :attribute must be accepted";
+
+    public function check($value, array $params)
+    {
+        $acceptables = ['yes', 'on', '1', 1, true, 'true'];
+        return in_array($value, $acceptables, true);
+    }
+
+}
---
+++ tests/Rules/AcceptedTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use Rakit\Validation\Rules\Accepted;
+
+class AcceptedTest extends PHPUnit_Framework_TestCase
+{
+
+    public function setUp()
+    {
+        $this->rule = new Accepted;
+    }
+
+    public function testValids()
+    {
+        $this->assertTrue($this->rule->check('yes', []));
+        $this->assertTrue($this->rule->check('on', []));
+        $this->assertTrue($this->rule->check('1', []));
+        $this->assertTrue($this->rule->check(1, []));
+        $this->assertTrue($this->rule->check(true, []));
+        $this->assertTrue($this->rule->check('true', []));
+    }
+
+    public function testInvalids()
+    {
+        $this->assertFalse($this->rule->check('', []));
+        $this->assertFalse($this->rule->check('onn', []));
+        $this->assertFalse($this->rule->check(' 1', []));
+        $this->assertFalse($this->rule->check(10, []));
+    }
+
+}
