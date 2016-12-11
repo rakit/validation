11d45a5395f3fa5b25a2ae005f377b2f97463cf5
Lanre Adelowo <me@lanreadelowo.com>
2016-12-11 10:04:15 +0100

    Added new rules to the baseValidators

--- src/Validator.php
+++ src/Validator.php
@@ -2,6 +2,9 @@
 
 namespace Rakit\Validation;
 
+use Rakit\Validation\Rules\After;
+use Rakit\Validation\Rules\Before;
+
 class Validator
 {
 
@@ -86,6 +89,8 @@ class Validator
             'present'           => new Rules\Present,
             'different'         => new Rules\Different,
             'uploaded_file'     => new Rules\UploadedFile,
+            'before'            => new Before,
+            'after'             => new After
         ];
 
         foreach($base_validators as $key => $validator) {
