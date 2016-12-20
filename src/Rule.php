f2ebdc0b88b66647398871bb7546e9e3fa004940
Muhammad Syifa <emsifa@gmail.com>
2016-12-20 07:10:35 +0700

    add setParameter and setParameters method

--- src/Rule.php
+++ src/Rule.php
@@ -52,6 +52,18 @@ abstract class Rule
         return $this->params;
     }
 
+    public function setParameters(array $params)
+    {
+        $this->params = array_merge($this->params, $params);
+        return $this;
+    }
+
+    public function setParameter($key, $value)
+    {
+        $this->params[$key] = $value;
+        return $this;
+    }
+
     public function fillParameters(array $params)
     {
         foreach($this->fillable_params as $key) {
