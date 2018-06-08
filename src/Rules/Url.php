ea58b031a350ba5f1a797db0767a7c12044c0aa6
Muhammad Syifa <emsifa@gmail.com>
2018-06-08 23:29:16 +0700

    Enhance URL validation by also checking it's scheme

--- src/Rules/Url.php
+++ src/Rules/Url.php
@@ -9,9 +9,64 @@ class Url extends Rule
 
     protected $message = "The :attribute is not valid url";
 
+    public function fillParameters(array $params)
+    {
+        if (count($params) == 1 AND is_array($params[0])) {
+            $params = $params[0];
+        }
+        return $this->forScheme($params);
+    }
+
+    public function forScheme($schemes)
+    {
+        $this->params['schemes'] = (array) $schemes;
+        return $this;
+    }
+
     public function check($value)
     {
+        $schemes = $this->parameter('schemes');
+
+        if (!$schemes) {
+            return $this->validateCommonScheme($value);
+        } else {
+            foreach ($schemes as $scheme) {
+                $method = 'validate' . ucfirst($scheme) .'Scheme';
+                if (method_exists($this, $method)) {
+                    if ($this->{$method}($value)) {
+                        return true;
+                    }
+                } elseif($this->validateCommonScheme($value, $scheme)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    public function validateBasic($value)
+    {
         return filter_var($value, FILTER_VALIDATE_URL) !== false;
+    }
+
+    public function validateCommonScheme($value, $scheme = null)
+    {
+        if (!$scheme) {
+            return $this->validateBasic($value) && (bool) preg_match("/^\w+:\/\//i", $value);
+        } else {
+            return $this->validateBasic($value) && (bool) preg_match("/^{$scheme}:\/\//", $value);
+        }
+    }
+
+    public function validateMailtoScheme($value)
+    {
+        return $this->validateBasic($value) && preg_match("/^mailto:/", $value);
+    }
+
+    public function validateJdbcScheme($value)
+    {
+        return (bool) preg_match("/^jdbc:\w+:\/\//", $value);
     }
 
 }
--- tests/Rules/UrlTest.php
+++ tests/Rules/UrlTest.php
@@ -12,15 +12,40 @@ class UrlTest extends PHPUnit_Framework_TestCase
 
     public function testValids()
     {
+        // Without specific schemes
         $this->assertTrue($this->rule->check('ftp://foobar.com'));
+        $this->assertTrue($this->rule->check('any://foobar.com'));
         $this->assertTrue($this->rule->check('http://foobar.com'));
         $this->assertTrue($this->rule->check('https://foobar.com'));
         $this->assertTrue($this->rule->check('https://foobar.com/path?a=123&b=blah'));
+
+        // Using specific schemes
+        $this->assertTrue($this->rule->fillParameters(['ftp'])->check('ftp://foobar.com'));
+        $this->assertTrue($this->rule->fillParameters(['any'])->check('any://foobar.com'));
+        $this->assertTrue($this->rule->fillParameters(['http'])->check('http://foobar.com'));
+        $this->assertTrue($this->rule->fillParameters(['https'])->check('https://foobar.com'));
+        $this->assertTrue($this->rule->fillParameters(['http', 'https'])->check('https://foobar.com'));
+        $this->assertTrue($this->rule->fillParameters(['foo', 'bar'])->check('bar://foobar.com'));
+        $this->assertTrue($this->rule->fillParameters(['mailto'])->check('mailto:johndoe@gmail.com'));
+        $this->assertTrue($this->rule->fillParameters(['jdbc'])->check('jdbc:mysql://localhost/dbname'));
+
+        // Using forScheme
+        $this->assertTrue($this->rule->forScheme('ftp')->check('ftp://foobar.com'));
+        $this->assertTrue($this->rule->forScheme('http')->check('http://foobar.com'));
+        $this->assertTrue($this->rule->forScheme('https')->check('https://foobar.com'));
+        $this->assertTrue($this->rule->forScheme(['http', 'https'])->check('https://foobar.com'));
+        $this->assertTrue($this->rule->forScheme('mailto')->check('mailto:johndoe@gmail.com'));
+        $this->assertTrue($this->rule->forScheme('jdbc')->check('jdbc:mysql://localhost/dbname'));
     }
 
     public function testInvalids()
     {
         $this->assertFalse($this->rule->check('foo:'));
+        $this->assertFalse($this->rule->check('mailto:johndoe@gmail.com'));
+        $this->assertFalse($this->rule->forScheme('mailto')->check('http://www.foobar.com'));
+        $this->assertFalse($this->rule->forScheme('ftp')->check('http://www.foobar.com'));
+        $this->assertFalse($this->rule->forScheme('jdbc')->check('http://www.foobar.com'));
+        $this->assertFalse($this->rule->forScheme(['http', 'https'])->check('any://www.foobar.com'));
     }
 
 }
