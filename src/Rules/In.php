6ac5ca1dd5840234071cce14c036d9d888eb6760
Muhammad Syifa <emsifa@gmail.com>
2018-08-06 14:17:02 +0700

    Add method to make In and NotIn to be strict

--- src/Rules/In.php
+++ src/Rules/In.php
@@ -9,6 +9,8 @@ class In extends Rule
 
     protected $message = "The :attribute is not allowing :value";
 
+    protected $strict = false;
+
     public function fillParameters(array $params)
     {
         if (count($params) == 1 AND is_array($params[0])) {
@@ -18,12 +20,17 @@ class In extends Rule
         return $this;
     }
 
+    public function strict($strict = true)
+    {
+        $this->strict = $strict;
+    }
+
     public function check($value)
     {
         $this->requireParameters(['allowed_values']);
 
         $allowed_values = $this->parameter('allowed_values');
-        return in_array($value, $allowed_values);
+        return in_array($value, $allowed_values, $this->strict);
     }
 
 }
