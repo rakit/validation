4df528be65f208a8f44f01ef22b49e612f2efd9d
Jay-r Simpron <ssj.simpron@gmail.com>
2020-07-29 19:39:26 +0800

    Update SizeTrait.php
    is_float should be is_numeric to recognize the numeric string as number

--- src/Rules/Traits/SizeTrait.php
+++ src/Rules/Traits/SizeTrait.php
@@ -15,7 +15,7 @@ trait SizeTrait
      */
     protected function getValueSize($value)
     {
-        if (is_int($value) || is_float($value)) {
+        if (is_int($value) || is_numeric($value)) {
             return (float) $value;
         } elseif (is_string($value)) {
             return (float) mb_strlen($value, 'UTF-8');
